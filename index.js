f7fe0cc07469432ca9260fbc4de613e99f190586
vwez <desirv63@gmail.com>
2023-08-18 01:36:00 +0200

    premier pour la creation du user endpoint

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 require('dotenv').config();
 const { swaggerUi, specs } = require('./docs/swagger');
-
+const bodyParser = require('body-parser')
 
 const app = express();
+
+const userRoutes = require('./routes/userRoutes')
+
+
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 
 // Intégrer Swagger UI à votre application
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/user', userRoutes);
 
 
 const PORT = process.env.PORT || 4000;

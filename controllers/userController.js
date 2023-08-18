02064c10810a57dcfb65bb0f6f2d02356e6f867d
vwez <desirv63@gmail.com>
2023-08-18 16:41:24 +0200

    vorrection du userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,103 +1,110 @@
-const db = require('../db')
-const argon2 = require('argon2')
-var jwt = require('jsonwebtoken')
+const db = require('../db');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const date = new Date();
+// Récupérer la date du jour au format jour/mois/année
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+const formattedToday = `${day}/${month}/${year}`;
 
-today = `${day}/${month}/${year}`
-
-
-const UserRegister = async(req, res) => {
+// Contrôleur d'inscription d'utilisateur
+const UserRegister = async (req, res) => {
     const body = req.body;
 
-    let select = "SELECT * FROM user WHERE email = ?;"
-    let connect = db.connection()
+    const selectQuery = "SELECT * FROM user WHERE email = ?;";
+    const insertQuery = "INSERT INTO user (firstname, lastname, email, phone, password, CreatedAt, UpdatedAt, IsVerify, role) VALUES (?,?,?,?,?,?,?,?,?);";
 
-    const hash = await argon2.hash(body.password);
-
-    try {
-        // on lui demande de promesse afin de savoir si il va bien renvoyer les données
-        await new Promise((resolve, reject) => {
-            // si l'execution a bien eu lieu 
-            let result = connect.execute(select,[body.email],  function(err, results, fields) {
-                // si lemail existe déjà on renvoi erreur dans le catch
-                if (results.length > 0) {
-                    return reject(true)
-                }
-                // sinon il continue son bout de chemin
-                return resolve(false)
-            })
-    
-        })
-    } catch (error) {
-        return res.status(409).json({
-            error: true,
-            message: ["L'utilisateur a déjà un compte"]
-        })
-    } 
-
-    let sql = "INSERT INTO user (firstname, lastname, email, phone, password, CreatedAt, UpdatedAt, IsVerify, role) VALUES (?,?,?,?,?,?;?,?,?);"
-    let array = [body.firstname, body.lastname, body.email, body.phone, hash,today, today, false, 'customer' ]
-    let requete = userModel.select(sql, array, connect)
-    db.disconnect(connect)
-
-    // rechercher les emails correspondant et mettre une erreur qui dit que l'email existe déjà 
-    
-    return res.status(200).json({
-        error: true,
-        message: ['']
-    })
-}
-
-
-
-const UserLogin = async(req, res) => {
-    const body = req.body;
-
-    let select = "SELECT * FROM user WHERE email = ?;"
-    let connect = db.connection();
+    const connect = db.connection();
 
     try {
-        // on lui demande de promesse afin de savoir si il va bien renvoyer les données
-        const resultat = await new Promise((resolve, reject) => {
-            // si l'execution a bien eu lieu 
-            
-            let result = connect.execute(select,[body.email],  function(err, results, fields) {
-                // si lemail existe déjà on renvoi erreur dans le catch
-                // console.log(results)
-                if (results.length > 0) {
-                    return resolve(results)
+        // Vérification de l'existence de l'email
+        const results = await new Promise((resolve, reject) => {
+            connect.execute(selectQuery, [body.email], function (err, results, fields) {
+                if (err) {
+                    return reject(err);
                 }
-                // sinon il continue son bout de chemin
-                return reject(true)
-            })
-    
-        })
-        // console.log(resultat)
-        if (!await argon2.verify(resultat[0].password, body.password)) {
+                return resolve(results);
+            });
+        });
+
+        if (results.length > 0) {
             return res.status(409).json({
                 error: true,
-                message: ["Password/User incorrecte"]
-            })
-        } 
+                message: ["L'utilisateur a déjà un compte"]
+            });
+        }
 
-        var token = jwt.sign({ email: resultat[0].email }, process.env.JWT_SECRET_KEY);
+        const hash = await argon2.hash(body.password);
+        const values = [body.firstname, body.lastname, body.email, body.phone, hash, formattedToday, formattedToday, false, 'customer'];
+        
+        const insertResult = await new Promise((resolve, reject) => {
+            connect.execute(insertQuery, values, function (err, results, fields) {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(results);
+            });
+        });
+
+        return res.status(200).json({
+            error: false,
+            message: ['Utilisateur inscrit avec succès']
+        });
+
     } catch (error) {
-        return res.status(409).json({
+        return res.status(500).json({
             error: true,
-            message: ["Password/User incorrecte"],
-        })
-    } 
+            message: ["Une erreur est survenue lors de l'inscription"]
+        });
+    } finally {
+        db.disconnect(connect);
+    }
+};
 
+// Contrôleur de connexion d'utilisateur
+const UserLogin = async (req, res) => {
+    const body = req.body;
 
-    // rechercher les emails correspondant et mettre une erreur qui dit que l'email existe déjà 
-    
-    return res.status(200).json({
-        error: true,
-        message: [''],
-        jwt : token
+    const selectQuery = "SELECT * FROM user WHERE email = ?;";
+    const connect = db.connection();
 
-    })
-}
-module.exports = {UserLogin, UserRegister}
+    try {
+        // Vérification de l'existence de l'email
+        const results = await new Promise((resolve, reject) => {
+            connect.execute(selectQuery, [body.email], function (err, results, fields) {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(results);
+            });
+        });
+
+        if (results.length === 0 || !(await argon2.verify(results[0].password, body.password))) {
+            return res.status(401).json({
+                error: true,
+                message: ["Mot de passe ou utilisateur incorrect"]
+            });
+        }
+
+        const token = jwt.sign({ email: results[0].email }, process.env.JWT_SECRET_KEY);
+
+        return res.status(200).json({
+            error: false,
+            message: ['Connexion réussie'],
+            jwt: token
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: ["Une erreur est survenue lors de la connexion"]
+        });
+    } finally {
+        db.disconnect(connect);
+    }
+};
+
+module.exports = { UserLogin, UserRegister };

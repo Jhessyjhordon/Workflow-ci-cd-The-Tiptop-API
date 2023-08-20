9bb52a8e573330a917f045c3ac11bc97ddd8f1cc
Jhessyjhordon <jhessyismael@gmail.com>
2023-08-21 00:44:30 +0200

    TEST 2

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 const PORT = process.env.PORT || 4000;
 
-// test pipeline from the api index.js
+// test pipeline from the api index.js 2
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
   });

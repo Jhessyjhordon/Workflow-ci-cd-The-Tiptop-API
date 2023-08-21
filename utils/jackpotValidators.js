de2faf38d39601093eb7d1bdba80e36802938c5e
vwez <desirv63@gmail.com>
2023-08-21 20:27:25 +0200

    well rename validators

--- utils/jackpotValidators.js
+++ utils/jackpotValidators.js
@@ -1,13 +1,13 @@
 const Yup = require('yup');
 
-const batchSchema = Yup.object({
+const jackpotchema = Yup.object({
     dateClientGagant: Yup.date().required(),
     idUser: Yup.number().required().positive().integer(),
     
 });
 
-const batchIdSchema = Yup.object({
+const jackpotIdSchema = Yup.object({
     id: Yup.number().required().positive().integer()
 });
 
-module.exports = { batchSchema, batchIdSchema};
+module.exports = { jackpotchema, jackpotIdSchema};

e7fbc661bbe1091f165a72971e32e4fc857e9159
vwez <desirv63@gmail.com>
2023-08-23 14:16:15 +0200

    rajout de l'attribut 'type_lot' pour les lots

--- utils/batchValidators.js
+++ utils/batchValidators.js
@@ -2,6 +2,7 @@ const Yup = require('yup');
 
 const batchSchema = Yup.object({
     valeur: Yup.number().required().positive(),
+    type_lot: Yup.string().required().min(1).max(155),
     description: Yup.string().required().min(1).max(255),
     pourcentage_gagnant: Yup.number().required().positive(),
     idUser: Yup.number().required().positive().integer(),

d258b2589f20602649d56dbac702eb9fd68adf56
vwez <desirv63@gmail.com>
2023-10-10 07:02:02 +0200

    use sequelize in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const db = require('../db');
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
+const User = require('../models/userModel'); // Assurez-vous que le chemin est correct
+
 require('dotenv').config();
 
 // Récupérer la date du jour au format jour/mois/année
@@ -14,23 +16,11 @@ const formattedToday = `${day}/${month}/${year}`;
 const UserRegister = async (req, res) => {
     const body = req.body;
 
-    const selectQuery = "SELECT * FROM user WHERE email = ?;";
-    const insertQuery = "INSERT INTO user (firstname, lastname, email, phone, password, CreatedAt, UpdatedAt, IsVerify, role) VALUES (?,?,?,?,?,?,?,?,?);";
-
-    const connect = db.connection();
-
     try {
         // Vérification de l'existence de l'email
-        const results = await new Promise((resolve, reject) => {
-            connect.execute(selectQuery, [body.email], function (err, results, fields) {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(results);
-            });
-        });
+        const existingUser = await User.findOne({ where: { email: body.email } });
 
-        if (results.length > 0) {
+        if (existingUser) {
             return res.status(409).json({
                 error: true,
                 message: ["L'utilisateur a déjà un compte"]
@@ -38,15 +28,16 @@ const UserRegister = async (req, res) => {
         }
 
         const hash = await argon2.hash(body.password);
-        const values = [body.firstname, body.lastname, body.email, body.phone, hash, formattedToday, formattedToday, false, 'customer'];
-        
-        const insertResult = await new Promise((resolve, reject) => {
-            connect.execute(insertQuery, values, function (err, results, fields) {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(results);
-            });
+        const newUser = await User.create({
+            firstName: body.firstname,
+            lastName: body.lastname,
+            email: body.email,
+            phone: body.phone,
+            password: hash,
+            CreatedAt: new Date(),
+            UpdatedAt: new Date(),
+            isVerify: false,
+            role: 'customer' // Vous pouvez modifier le rôle selon vos besoins
         });
 
         return res.status(200).json({
@@ -55,41 +46,31 @@ const UserRegister = async (req, res) => {
         });
 
     } catch (error) {
+        console.error('Erreur lors de l\'inscription avec Sequelize :', error);
         return res.status(500).json({
             error: true,
             message: ["Une erreur est survenue lors de l'inscription"]
         });
-    } finally {
-        db.disconnect(connect);
     }
 };
+
 
 // Contrôleur de connexion d'utilisateur
 const UserLogin = async (req, res) => {
     const body = req.body;
 
-    const selectQuery = "SELECT * FROM user WHERE email = ?;";
-    const connect = db.connection();
-
     try {
-        // Vérification de l'existence de l'email
-        const results = await new Promise((resolve, reject) => {
-            connect.execute(selectQuery, [body.email], function (err, results, fields) {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(results);
-            });
-        });
+        // Vérification de l'existence de l'email et récupération de l'utilisateur
+        const user = await User.findOne({ where: { email: body.email } });
 
-        if (results.length === 0 || !(await argon2.verify(results[0].password, body.password))) {
+        if (!user || !(await argon2.verify(user.password, body.password))) {
             return res.status(401).json({
                 error: true,
                 message: ["Mot de passe ou utilisateur incorrect"]
             });
         }
 
-        const token = jwt.sign({ email: results[0].email, role: results[0].role }, process.env.JWT_SECRET_KEY);
+        const token = jwt.sign({ email: user.email, role: user.role }, process.env.JWT_SECRET_KEY);
 
         return res.status(200).json({
             error: false,
@@ -98,28 +79,24 @@ const UserLogin = async (req, res) => {
         });
 
     } catch (error) {
-        console.log(error);
+        console.error('Erreur lors de la connexion avec Sequelize :', error);
         return res.status(500).json({
             error: true,
             message: ["Une erreur est survenue lors de la connexion"]
         });
-    } finally {
-        db.disconnect(connect);
     }
 };
 
 // Contrôleur pour la route GET '/'
 const getAllUsers = async (req, res) => {
     const token = req.headers.authorization; // Récupérer le token de l'en-tête
-    const selectQuery = "SELECT * FROM user;";
-    const connect = db.connection();
 
     try {
         // Vérifier la présence du token
         if (!token) {
             return res.status(401).json({
                 error: true,
-                message: ["Accès non autorisé"] //Token manquant
+                message: ["Accès non autorisé"] // Token manquant
             });
         }
 
@@ -134,34 +111,27 @@ const getAllUsers = async (req, res) => {
             });
         }
 
-        const results = await new Promise((resolve, reject) => {
-            connect.execute(selectQuery, function (err, results, fields) {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(results);
-            });
-        });
+        // Récupérer la liste des utilisateurs
+        const users = await User.findAll();
 
         return res.status(200).json({
             error: false,
             message: ['Liste des utilisateurs'],
-            users: results
+            users
         });
 
     } catch (error) {
         if (error.name === 'TokenExpiredError') {
             return res.status(401).json({
                 error: true,
-                message: ["Veillez vous reconnecter"] //Token expiré
+                message: ["Veillez vous reconnecter"] // Token expiré
             });
         }
+        console.error('Erreur lors de la récupération des utilisateurs avec Sequelize :', error);
         return res.status(500).json({
             error: true,
             message: ["Une erreur est survenue lors de la récupération des utilisateurs"]
         });
-    } finally {
-        db.disconnect(connect);
     }
 };
 
@@ -173,18 +143,26 @@ const getAllUsers = async (req, res) => {
 const updateUserById = async (req, res) => {
     const userId = req.params.id;
     const body = req.body;
-    const updateQuery = "UPDATE user SET firstname = ?, lastname = ?, email = ?, phone = ? WHERE id = ?;";
-    const connect = db.connection();
 
     try {
-        const updateResult = await new Promise((resolve, reject) => {
-            connect.execute(updateQuery, [body.firstname, body.lastname, body.email, body.phone, userId], function (err, results, fields) {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(results);
+        // Rechercher l'utilisateur par ID
+        const userToUpdate = await User.findByPk(userId);
+
+        if (!userToUpdate) {
+            return res.status(404).json({
+                error: true,
+                message: ["Utilisateur non trouvé"]
             });
-        });
+        }
+
+        // Mettre à jour les champs de l'utilisateur
+        userToUpdate.firstName = body.firstname;
+        userToUpdate.lastName = body.lastname;
+        userToUpdate.email = body.email;
+        userToUpdate.phone = body.phone;
+
+        // Enregistrer les modifications dans la base de données
+        await userToUpdate.save();
 
         return res.status(200).json({
             error: false,
@@ -192,31 +170,23 @@ const updateUserById = async (req, res) => {
         });
 
     } catch (error) {
+        console.error('Erreur lors de la mise à jour de l\'utilisateur avec Sequelize :', error);
         return res.status(500).json({
             error: true,
             message: ["Une erreur est survenue lors de la mise à jour de l'utilisateur"]
         });
-    } finally {
-        db.disconnect(connect);
     }
 };
 
+// Contrôleur pour la route GET '/:id'
 const getUserById = async (req, res) => {
     const userId = req.params.id;
-    const selectQuery = "SELECT * FROM user WHERE id = ?;";
-    const connect = db.connection();
 
     try {
-        const results = await new Promise((resolve, reject) => {
-            connect.execute(selectQuery, [userId], function (err, results, fields) {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(results);
-            });
-        });
+        // Rechercher l'utilisateur par ID
+        const user = await User.findByPk(userId);
 
-        if (results.length === 0) {
+        if (!user) {
             return res.status(404).json({
                 error: true,
                 message: ["Utilisateur non trouvé"]
@@ -226,47 +196,47 @@ const getUserById = async (req, res) => {
         return res.status(200).json({
             error: false,
             message: ['Utilisateur trouvé'],
-            ticket: results[0]
+            user
         });
 
     } catch (error) {
+        console.error('Erreur lors de la récupération de l\'utilisateur avec Sequelize :', error);
         return res.status(500).json({
             error: true,
             message: ["Une erreur est survenue lors de la récupération de l'utilisateur"]
         });
-    } finally {
-        db.disconnect(connect);
     }
 };
 
 
 const deleteUserById = async (req, res) => {
-    const ticketId = req.params.id;
-    const deleteQuery = "DELETE FROM user WHERE id = ?;";
-    const connect = db.connection();
+    const userId = req.params.id;
 
     try {
-        const deleteResult = await new Promise((resolve, reject) => {
-            connect.execute(deleteQuery, [ticketId], function (err, results, fields) {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(results);
+        // Rechercher l'utilisateur par ID
+        const userToDelete = await User.findByPk(userId);
+
+        if (!userToDelete) {
+            return res.status(404).json({
+                error: true,
+                message: ["Utilisateur non trouvé"]
             });
-        });
+        }
+
+        // Supprimer l'utilisateur de la base de données
+        await userToDelete.destroy();
 
         return res.status(200).json({
             error: false,
-            message: ['Utlisateur supprimé avec succès']
+            message: ['Utilisateur supprimé avec succès']
         });
 
     } catch (error) {
+        console.error('Erreur lors de la suppression de l\'utilisateur avec Sequelize :', error);
         return res.status(500).json({
             error: true,
             message: ["Une erreur est survenue lors de la suppression de l'utilisateur"]
         });
-    } finally {
-        db.disconnect(connect);
     }
 };
 module.exports = { UserLogin, UserRegister, getUserById, deleteUserById, updateUserById, getAllUsers};

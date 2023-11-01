b593fa260d89ac0929f20976e2aba7687145a1a4
Alexandre HUYNH <alexandre.huynh98@gmail.com>
2023-11-01 16:32:39 +0000

    Remodification pour autoriser que l'admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -187,7 +187,7 @@ const getAllUsers = async (req, res) => {
         const decodedToken = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET_KEY);
 
         // Vérifier le rôle de l'utilisateur (assumons que le rôle est stocké dans decodedToken.role)
-        if (decodedToken.role !== 'client') {
+        if (decodedToken.role !== 'admin') {
             return res.status(403).json({
                 error: true,
                 message: ["Accès refusé"]

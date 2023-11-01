15ddbf61248bd34bee78813611fb71fca389896f
vwez <desirv63@gmail.com>
2023-11-01 12:48:15 +0100

    add all for sso login with google

--- controllers/userController.js
+++ controllers/userController.js
@@ -128,7 +128,7 @@ const UserLogin = async (req, res) => {
         }else {
             // Sinon, vérifions le mot de passe
             if (bcrypt.compareSync(body.password, user.password)) {
-                const token = jwt.sign({ email: user.email, role: user.role }, process.env.JWT_SECRET_KEY);
+                const token = jwt.sign({ email: user.email, id: user.id, role: user.role }, process.env.JWT_SECRET_KEY);
                 return res.status(200).json({
                     error: false,
                     message: ['Connexion réussie'],
@@ -372,4 +372,41 @@ const deleteUserById = async (req, res) => {
         });
     }
 };
-module.exports = { UserLogin, UserRegister, getUserById, deleteUserById, updateUserById, getAllUsers, getAllUsersByRoleClient, UserCreation};
+
+const GoogleAuth = async (req, res) => {
+    try {
+      // Les données de l'utilisateur renvoyées par Google après une authentification réussie
+      const userData = req.user;
+
+      // Récupérer l'email de l'utilisateur depuis les données de Google
+      const userEmail = userData.emails[0].value; // Supposons que l'email est la première valeur dans le tableau des emails
+      
+      const user = await User.findOne({ where: { email: userEmail } });
+
+      if (!user) {
+        return res.status(404).json({
+          error: true,
+          message: ["Utilisateur non trouvé"]
+        });
+      }
+
+      const token = jwt.sign({ email: user.email, id: user.id, role: user.role }, process.env.JWT_SECRET_KEY);
+      return res.status(200).json({
+        error: false,
+        message: ['Connexion réussie'],
+        jwt: token
+        });
+
+    //   res.redirect('/success');
+    } catch (error) {
+      console.error('Erreur lors de l\'authentification Google :', error);
+      // Gérer les erreurs ici
+      res.status(500).json({
+        error: true,
+        message: ["Erreur lors de l'authentification avec Google"]
+      });
+    }
+  }
+
+  
+module.exports = { UserLogin, UserRegister, getUserById, deleteUserById, updateUserById, getAllUsers, UserCreation, GoogleAuth};
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const { swaggerUi, specs } = require('./docs/swagger');
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
@@ -12,6 +15,12 @@ const batchRoutes = require('./routes/batchRoutes')
 const ticketRoutes = require('./routes/ticketRoutes')
 const jackpothRoutes = require('./routes/jackpotRoutes')
 
+// Configure session management
+app.use(session({ secret: process.env.SECRET_KEY, resave: true, saveUninitialized: true }));
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // app.use(cors({
 //   origin: 'http://api.dev.dsp-archiwebo22b-ji-rw-ah.fr/',
@@ -25,6 +34,26 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// Configure Google OAuth2.0
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:4000/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  // Handle user information and authentication here
+  // 'profile' contains user information
+  return done(null, profile);
+}));
+
+// Serialize user
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 app.get('/', function (req, res, next) {
   res.json({ msg: 'Welcome to th2 Tiptop Api documentation' })
 })
@@ -36,6 +65,19 @@ app.use('/shop', shopRoutes);
 app.use('/batch', batchRoutes);
 app.use('/ticket', ticketRoutes);
 app.use('/jackpot', jackpothRoutes);
+
+
+
+
+
+// Define your routes for Google SSO
+// app.get('/auth/google', passport.authenticate('google', { scope: ['email'] }));
+// app.get('/auth/google/callback',
+//     passport.authenticate('google', { failureRedirect: '/login' }),
+//     (req, res) => {
+//         // Successful authentication, redirect to a success page or generate JWT tokens.
+//         res.redirect('/success');
+//     });
 
 
 const PORT = process.env.PORT || 4000;

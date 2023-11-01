44c9acb999933aec5b6343067ddf6da8f5cad39e
Alexandre HUYNH <alexandre.huynh98@gmail.com>
2023-11-01 16:28:01 +0000

    Add /role/client to router.get

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -33,7 +33,7 @@ router.get('/', userMiddleware.checkIfUserToken ,userMiddleware.checkIfUserIsEmp
  *       200:
  *         description: Liste des utilisateurs récupérée avec succès
  */
-router.get('/', userMiddleware.checkIfUserToken ,userMiddleware.checkIfUserIsEmployee,userController.getAllUsersByRoleClient)
+router.get('/role/client', userMiddleware.checkIfUserToken ,userMiddleware.checkIfUserIsEmployee,userController.getAllUsersByRoleClient)
 
 /**
  * @swagger

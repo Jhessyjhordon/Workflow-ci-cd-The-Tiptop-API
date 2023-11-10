5b71203ac366b2296bc518bed75152f50acfaa66
aruseus98 <aruseus98@gmail.com>
2023-11-10 22:33:12 +0100

    S retiré du dossier test

--- .mocharc.js
+++ .mocharc.js
@@ -5,6 +5,6 @@ module.exports = {
     reporter: 'spec',
     timeout: 10000,
     exit: true,
-    file: ['/var/jenkins_home/workspace/the-tiptop-api-2_dev/the-tiptop-api-dev/tests/setup.js', '/var/jenkins_home/workspace/the-tiptop-api-2_dev/the-tiptop-api-dev/tests/*.test.js'], // Exécutez le script de configuration avant les tests
+    file: ['./test/setup.js', './test/*.test.js'], // Exécutez le script de configuration avant les tests
   };
   

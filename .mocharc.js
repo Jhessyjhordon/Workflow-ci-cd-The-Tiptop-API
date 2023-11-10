9171d40358c5340ca16591d3e5ad71f35dcccb9d
aruseus98 <aruseus98@gmail.com>
2023-11-10 22:30:24 +0100

    Test avec chemin absolu

--- .mocharc.js
+++ .mocharc.js
@@ -5,6 +5,6 @@ module.exports = {
     reporter: 'spec',
     timeout: 10000,
     exit: true,
-    file: ['./tests/setup.js', './tests/*.test.js'], // Exécutez le script de configuration avant les tests
+    file: ['/var/jenkins_home/workspace/the-tiptop-api-2_dev/the-tiptop-api-dev/tests/setup.js', '/var/jenkins_home/workspace/the-tiptop-api-2_dev/the-tiptop-api-dev/tests/*.test.js'], // Exécutez le script de configuration avant les tests
   };
   

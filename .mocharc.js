afd5f06307956830a4cb68faf78ea65a868af78b
aruseus98 <aruseus98@gmail.com>
2023-11-13 18:38:45 +0100

    Modify sonar properties files V2.5

--- .mocharc.js
+++ .mocharc.js
@@ -2,7 +2,12 @@
 module.exports = {
     require: 'dotenv/config', // Pour charger les variables d'environnement avec dotenv
     recursive: true,
-    reporter: 'mocha-junit-reporter',
+    reporter: 'mocha-sonarqube-reporter', // Reporter compatible pour Sonarqube
+    reporterOptions:{
+      output : 'unit-tests.xml',// default to ./xunit.xml
+      useFullFilePath: 'true' // default to 'false'. Uses full test file paths in the report.
+    },
+    quiet: true,
     timeout: 10000,
     exit: true,
     file: ['./tests/setup.js'], // Exécutez le script de configuration avant les tests

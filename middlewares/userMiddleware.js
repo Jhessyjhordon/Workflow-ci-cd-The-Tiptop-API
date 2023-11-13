78eaf0907cc598e24aa06f2063e39fb3868f0574
aruseus98 <aruseus98@gmail.com>
2023-11-13 17:59:17 +0100

    Test mocha 24 succès - 1 fail V1.1

--- middlewares/userMiddleware.js
+++ middlewares/userMiddleware.js
@@ -88,7 +88,7 @@ const checkIfUserIsAdmin = (req, res, next) => {
   if (token.role !== 'admin') {
       return res.status(403).json({
           error: true,
-          message: ["Accès refusé : vous n'êtes pas autorisé à effectuer cette action."]
+          message: "Accès refusé : vous n'êtes pas autorisé à effectuer cette action."
       });
   }
   next();
@@ -100,7 +100,7 @@ const checkIfUserIsEmployeeOrAdmin = (req, res, next) => {
   if (token.role !== 'employee' && token.role !== 'admin') {
       return res.status(403).json({
           error: true,
-          message: ["Accès refusé : vous n'êtes pas autorisé à effectuer cette action."]
+          message: "Accès refusé : vous n'êtes pas autorisé à effectuer cette action."
       });
   }
   next();

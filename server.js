881c1e093ef9e36b6a06bab049390182e6f3f4cd
aruseus98 <aruseus98@gmail.com>
2023-11-26 15:21:00 +0100

    Ajout console log

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const express = require('express')
 // Définir un port par défaut
 const DEFAULT_PORT = 4000;
 const PORT = process.env.PORT || DEFAULT_PORT;
-
+console.log('NODE_ENV lors du déploiement:', process.env.NODE_ENV);
 // Démarre le serveur uniquement si nous ne sommes pas en train de tester
 if (process.env.NODE_ENV !== 'dev') {
   app.listen(PORT, () => {
+    
     console.log(`Serveur démarré sur le port ${PORT}`);
     module.exports = app;
   });

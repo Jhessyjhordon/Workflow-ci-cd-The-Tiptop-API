7bd6c0ed4b74e26dcb106c5dcc7fe46cf0457f6e
vwez <desirv63@gmail.com>
2023-11-27 01:14:04 +0100

    add scope on passport config

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  scope: ['profile', 'email'],
 }, (accessToken, refreshToken, profile, done) => {
   // Handle user information and authentication here
   // 'profile' contains user information

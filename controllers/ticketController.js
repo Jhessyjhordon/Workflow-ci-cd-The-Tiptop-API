a97c6abeaaaa88da9a8c60e63ee50099cc21339e
Jhessyjhordon <jhessyismael@gmail.com>
2023-11-29 20:06:06 +0100

    correction de const data dans verifyTicketUserId encore

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -84,29 +84,36 @@ const verifyTicketUserId = async (req, res) => {
     if (!tickets || tickets.length === 0) {
       return res.status(404).json({
         error: true,
-        message: ["Aucun ticket non trouvé"]
+        message: ["Aucun ticket trouvé"]
       });
     }
 
-    const user = await User.findByPk(tickets.user_id);
+    // Utilisez Promise.all pour exécuter toutes les requêtes en parallèle
+    const userPromises = tickets.map(ticket => User.findByPk(ticket.user_id));
 
-    if (!user) {
+    // Attendez que toutes les promesses soient résolues
+    const users = await Promise.all(userPromises);
+
+    // Vérifiez si au moins un utilisateur a été trouvé
+    if (users.some(user => !user)) {
       return res.status(404).json({
         error: true,
-        message: ["Ce Ticket n'appartient à aucun Client"]
+        message: ["Certains tickets n'appartiennent à aucun Client"]
       });
     }
 
-    const batch = await Batch.findByPk(tickets.batch_id);
-
-    if (!batch) {
+    const batchesPromises = tickets.map(ticket => Batch.findByPk(ticket.batch_id));
+    const batches = await Promise.all(batchesPromises);
+    
+    // Vérifiez si au moins un lot a été trouvé
+    if (batches.some(batch => !batch)) {
       return res.status(404).json({
         error: true,
-        message: ["Aucun lot n'est lié à ce ticket"]
+        message: ["Certains tickets n'ont aucun lot lié"]
       });
     }
 
-    const data = tickets.map(ticket => ({
+    const data = tickets.map((ticket, index) => ({
       "id": ticket.id,
       "numTicket": ticket.numTicket,
       "montantAchat": ticket.montantAchat,
@@ -114,17 +121,17 @@ const verifyTicketUserId = async (req, res) => {
       "statusGain": ticket.statusGain,
       "state": "checked", // Tous les tickets ont maintenant le statut "checked"
       "user": {
-        "id": user.id,
-        "lastname": user.lastname,
-        "firstname": user.firstname,
-        "email": user.email,
-        "address": user.address,
+        "id": users[index].id,
+        "lastname": users[index].lastname,
+        "firstname": users[index].firstname,
+        "email": users[index].email,
+        "address": users[index].address,
       },
       "batch": {
-        "id": batch.id,
-        "type_lot": batch.type_lot,
-        "valeur": batch.valeur,
-        "description": batch.description
+        "id": batches[index].id,
+        "type_lot": batches[index].type_lot,
+        "valeur": batches[index].valeur,
+        "description": batches[index].description
       }
     }));
 
@@ -146,201 +153,201 @@ const verifyTicketUserId = async (req, res) => {
 
 
 const createTicket = async (req, res) => {
-    const body = req.body;
-  
-    try {
-      // Utilisez Sequelize pour créer un nouvel enregistrement de ticket
-      const newTicket = await Ticket.create({
-        numTicket: body.numTicket,
-        montantTicket: body.montantTicket,
-        dateAchat: body.dateAchat,
-        gainAttribue: body.gainAttribue,
-        // statusGain: body.statusGain,
-        batchId: body.batchId,
-        userId: body.userId,
-        createAt: new Date(),
-        updatedAt: new Date()
-      });
-  
-      return res.status(201).json({
-        error: false,
-        message: ['Ticket enregistré avec succès'],
-        ticket: newTicket
-      });
-  
-    } catch (error) {
-      console.error('Erreur lors de la création du ticket avec Sequelize :', error);
-      return res.status(500).json({
+  const body = req.body;
+
+  try {
+    // Utilisez Sequelize pour créer un nouvel enregistrement de ticket
+    const newTicket = await Ticket.create({
+      numTicket: body.numTicket,
+      montantTicket: body.montantTicket,
+      dateAchat: body.dateAchat,
+      gainAttribue: body.gainAttribue,
+      // statusGain: body.statusGain,
+      batchId: body.batchId,
+      userId: body.userId,
+      createAt: new Date(),
+      updatedAt: new Date()
+    });
+
+    return res.status(201).json({
+      error: false,
+      message: ['Ticket enregistré avec succès'],
+      ticket: newTicket
+    });
+
+  } catch (error) {
+    console.error('Erreur lors de la création du ticket avec Sequelize :', error);
+    return res.status(500).json({
+      error: true,
+      message: ["Une erreur est survenue lors de l'enregistrement du ticket"]
+    });
+  }
+};
+
+const updateTicketById = async (req, res) => {
+  const ticketId = req.params.id;
+  const body = req.body;
+
+  try {
+    // Utilisez Sequelize pour trouver le ticket à mettre à jour par son ID
+    const ticketToUpdate = await Ticket.findByPk(ticketId);
+
+    if (!ticketToUpdate) {
+      return res.status(404).json({
         error: true,
-        message: ["Une erreur est survenue lors de l'enregistrement du ticket"]
+        message: ["Ticket non trouvé"]
       });
     }
-  };
 
-  const updateTicketById = async (req, res) => {
-    const ticketId = req.params.id;
-    const body = req.body;
-  
-    try {
-      // Utilisez Sequelize pour trouver le ticket à mettre à jour par son ID
-      const ticketToUpdate = await Ticket.findByPk(ticketId);
-  
-      if (!ticketToUpdate) {
-        return res.status(404).json({
-          error: true,
-          message: ["Ticket non trouvé"]
-        });
-      }
-  
-      // Effectuez la mise à jour des champs
-      ticketToUpdate.numTicket = body.numTicket;
-      ticketToUpdate.montantTicket = body.montantTicket;
-      ticketToUpdate.dateAchat = body.dateAchat;
-      ticketToUpdate.gainAttribue = body.gainAttribue;
-      ticketToUpdate.statusGain = body.statusGain;
-      ticketToUpdate.batchId = body.batchId;
-      ticketToUpdate.userId = body.userId;
-      ticketToUpdate.state = body.state;
-      ticketToUpdate.updatedAt = new Date();
-  
-      // Enregistrez les modifications dans la base de données
-      await ticketToUpdate.save();
-  
-      return res.status(200).json({
-        error: false,
-        message: ['Ticket mis à jour avec succès'],
-        ticket: ticketToUpdate
-      });
-  
-    } catch (error) {
-      console.error('Erreur lors de la mise à jour du ticket avec Sequelize :', error);
-      return res.status(500).json({
+    // Effectuez la mise à jour des champs
+    ticketToUpdate.numTicket = body.numTicket;
+    ticketToUpdate.montantTicket = body.montantTicket;
+    ticketToUpdate.dateAchat = body.dateAchat;
+    ticketToUpdate.gainAttribue = body.gainAttribue;
+    ticketToUpdate.statusGain = body.statusGain;
+    ticketToUpdate.batchId = body.batchId;
+    ticketToUpdate.userId = body.userId;
+    ticketToUpdate.state = body.state;
+    ticketToUpdate.updatedAt = new Date();
+
+    // Enregistrez les modifications dans la base de données
+    await ticketToUpdate.save();
+
+    return res.status(200).json({
+      error: false,
+      message: ['Ticket mis à jour avec succès'],
+      ticket: ticketToUpdate
+    });
+
+  } catch (error) {
+    console.error('Erreur lors de la mise à jour du ticket avec Sequelize :', error);
+    return res.status(500).json({
+      error: true,
+      message: ["Une erreur est survenue lors de la mise à jour du ticket"]
+    });
+  }
+};
+
+const deleteTicketById = async (req, res) => {
+  const ticketId = req.params.id;
+
+  try {
+    // Utilisez Sequelize pour supprimer le ticket par ID
+    const deleteCount = await Ticket.destroy({
+      where: { id: ticketId }
+    });
+
+    if (deleteCount === 0) {
+      return res.status(404).json({
         error: true,
-        message: ["Une erreur est survenue lors de la mise à jour du ticket"]
+        message: ["Ticket non trouvé"]
       });
     }
-  };
 
-  const deleteTicketById = async (req, res) => {
-    const ticketId = req.params.id;
-  
-    try {
-      // Utilisez Sequelize pour supprimer le ticket par ID
-      const deleteCount = await Ticket.destroy({
-        where: { id: ticketId }
-      });
-  
-      if (deleteCount === 0) {
-        return res.status(404).json({
-          error: true,
-          message: ["Ticket non trouvé"]
-        });
-      }
-  
-      return res.status(200).json({
-        error: false,
-        message: ['Ticket supprimé avec succès']
-      });
-  
-    } catch (error) {
-      console.error('Erreur lors de la suppression du ticket avec Sequelize :', error);
-      return res.status(500).json({
+    return res.status(200).json({
+      error: false,
+      message: ['Ticket supprimé avec succès']
+    });
+
+  } catch (error) {
+    console.error('Erreur lors de la suppression du ticket avec Sequelize :', error);
+    return res.status(500).json({
+      error: true,
+      message: ["Une erreur est survenue lors de la suppression du ticket"]
+    });
+  }
+};
+
+const getAllTickets = async (req, res) => {
+  try {
+    // Utilisez Sequelize pour récupérer tous les tickets
+    const tickets = await Ticket.findAll();
+
+    if (tickets.length === 0) {
+      return res.status(404).json({
         error: true,
-        message: ["Une erreur est survenue lors de la suppression du ticket"]
+        message: ["Aucun ticket trouvé"]
       });
     }
-  };
 
-  const getAllTickets = async (req, res) => {
-    try {
-      // Utilisez Sequelize pour récupérer tous les tickets
-      const tickets = await Ticket.findAll();
-  
-      if (tickets.length === 0) {
-        return res.status(404).json({
-          error: true,
-          message: ["Aucun ticket trouvé"]
-        });
-      }
-  
-      return res.status(200).json({
-        error: false,
-        message: ['Liste des Tickets'],
-        tickets
-      });
-  
-    } catch (error) {
-      console.error('Erreur lors de la récupération des tickets avec Sequelize :', error);
-      return res.status(500).json({
+    return res.status(200).json({
+      error: false,
+      message: ['Liste des Tickets'],
+      tickets
+    });
+
+  } catch (error) {
+    console.error('Erreur lors de la récupération des tickets avec Sequelize :', error);
+    return res.status(500).json({
+      error: true,
+      message: ["Une erreur est survenue lors de la récupération des Tickets"]
+    });
+  }
+};
+
+const getTicketById = async (req, res) => {
+  const ticketId = req.params.id;
+
+  try {
+    // Utilisez Sequelize pour rechercher le ticket par ID
+    const ticket = await Ticket.findByPk(ticketId);
+
+    if (!ticket) {
+      return res.status(404).json({
         error: true,
-        message: ["Une erreur est survenue lors de la récupération des Tickets"]
+        message: ["Ticket non trouvé"]
       });
     }
-  };
 
-  const getTicketById = async (req, res) => {
-    const ticketId = req.params.id;
-  
-    try {
-      // Utilisez Sequelize pour rechercher le ticket par ID
-      const ticket = await Ticket.findByPk(ticketId);
-  
-      if (!ticket) {
-        return res.status(404).json({
-          error: true,
-          message: ["Ticket non trouvé"]
-        });
-      }
-  
-      return res.status(200).json({
-        error: false,
-        message: ['Ticket trouvé'],
-        ticket
-      });
-    } catch (error) {
-      console.error('Erreur lors de la récupération du ticket avec Sequelize :', error);
-      return res.status(500).json({
+    return res.status(200).json({
+      error: false,
+      message: ['Ticket trouvé'],
+      ticket
+    });
+  } catch (error) {
+    console.error('Erreur lors de la récupération du ticket avec Sequelize :', error);
+    return res.status(500).json({
+      error: true,
+      message: ["Une erreur est survenue lors de la récupération du ticket"]
+    });
+  }
+};
+
+const partialUpdateTicketById = async (req, res) => {
+  const ticketId = req.params.id;
+  const body = req.body;
+
+  try {
+    // Utilisez Sequelize pour trouver le ticket à mettre à jour par son ID
+    const ticketToUpdate = await Ticket.findByPk(ticketId);
+
+    if (!ticketToUpdate) {
+      return res.status(404).json({
         error: true,
-        message: ["Une erreur est survenue lors de la récupération du ticket"]
+        message: ["Ticket non trouvé"]
       });
     }
-  };
 
-  const partialUpdateTicketById = async (req, res) => {
-    const ticketId = req.params.id;
-    const body = req.body;
-  
-    try {
-      // Utilisez Sequelize pour trouver le ticket à mettre à jour par son ID
-      const ticketToUpdate = await Ticket.findByPk(ticketId);
-  
-      if (!ticketToUpdate) {
-        return res.status(404).json({
-          error: true,
-          message: ["Ticket non trouvé"]
-        });
+    for (const [key, value] of Object.entries(body)) {
+      if (key in ticketToUpdate) {
+        ticketToUpdate[key] = value;
       }
-  
-      for (const [key, value] of Object.entries(body)) {
-        if (key in ticketToUpdate) {
-          ticketToUpdate[key] = value;
-        }
-      }
-  
-      // Enregistrez les modifications dans la base de données
-      await ticketToUpdate.save();
-  
-      return res.status(200).json({
-        error: false,
-        message: ['Ticket mis à jour avec succès']
-      });
-    } catch (error) {
-      console.error('Erreur de mise à jour de ticket avec Sequelize :', error);
-      return res.status(500).json({
-        error: true,
-        message: ["Une erreur est survenue lors de la mise à jour du ticket"]
-      });
     }
-  };
 
-module.exports = { createTicket, updateTicketById, deleteTicketById, getAllTickets, getTicketById, partialUpdateTicketById, verifyTicket, verifyTicketUserId};
+    // Enregistrez les modifications dans la base de données
+    await ticketToUpdate.save();
+
+    return res.status(200).json({
+      error: false,
+      message: ['Ticket mis à jour avec succès']
+    });
+  } catch (error) {
+    console.error('Erreur de mise à jour de ticket avec Sequelize :', error);
+    return res.status(500).json({
+      error: true,
+      message: ["Une erreur est survenue lors de la mise à jour du ticket"]
+    });
+  }
+};
+
+module.exports = { createTicket, updateTicketById, deleteTicketById, getAllTickets, getTicketById, partialUpdateTicketById, verifyTicket, verifyTicketUserId };
